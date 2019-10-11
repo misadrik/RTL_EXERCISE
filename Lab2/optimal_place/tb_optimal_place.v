9756ba15d7298d4a3d8dc5e4bf615bf3b4d4ef58
MISaD <misad.chien@gmail.com>
2019-10-11 14:48:30 -0700

    add support to print result

--- Lab2/optimal_place/tb_optimal_place.v
+++ Lab2/optimal_place/tb_optimal_place.v
@@ -2,6 +2,10 @@
 module tb();
     reg CLK;
     reg RST;
+    wire    [2:0]     x_min;
+    wire    [2:0]     y_min;
+    wire    [15:0]    cost_mini;
+    wire              output_vld;
 
     parameter DUTY = 1;
 
@@ -14,6 +18,14 @@ module tb();
         RST = 1;
     end
 
-    optimal_place U_OPTIMAL_PLACE(.CLK(CLK), .RST_n(RST));
+    always @(posedge CLK or negedge RST) begin : proc_output_vld
+        if(output_vld == 1'b1) begin
+            $display("x = %d, y= %d, total_cost = %d",x_min, y_min, cost_mini);
+            $display("At_time: %t",$time);
+            $stop;
+        end
+    end
+
+    optimal_place U_OPTIMAL_PLACE(.CLK(CLK), .RST_n(RST),.x_min(x_min),.y_min(y_min),.cost_mini(cost_mini),.output_vld(output_vld));
     
 endmodule
